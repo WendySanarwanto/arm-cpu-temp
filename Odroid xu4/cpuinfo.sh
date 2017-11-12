24cf9968fb42d966f0baed5931f04b6b0c8974c0
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2017-11-13 00:50:06 +0800

    Removed space between degree and C

--- Odroid xu4/cpuinfo.sh
+++ Odroid xu4/cpuinfo.sh
@@ -7,9 +7,9 @@ for i in {0..7}
 do
     if [ "$i" -gt 3 ]; then
         j=$(($i-4))
-        echo "CPU-$i Frequency: "$((`sudo cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq` /1000)) ' Mhz. Temperature: ' $((`cat /sys/devices/virtual/thermal/thermal_zone$j/temp` /1000)) '° C' 
+        echo "CPU-$i Frequency: "$((`sudo cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq` /1000)) ' Mhz. Temperature: ' $((`cat /sys/devices/virtual/thermal/thermal_zone$j/temp` /1000)) '°C' 
     else
         echo "CPU-$i Frequency: "$((`sudo cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq` /1000)) ' Mhz.'
     fi
 done
-echo "GPU Temperature: "$((`cat /sys/devices/virtual/thermal/thermal_zone4/temp` /1000)) '° C'
+echo "GPU Temperature: "$((`cat /sys/devices/virtual/thermal/thermal_zone4/temp` /1000)) '°C'
